e3fa69e11e529fca7c39cd3a36d75aec048524b1
Donn Neufeld <69326497+ForeDaddy@users.noreply.github.com>
2022-11-16 13:50:36 -0800

    All Tests Pass Using .split .reverse .join Methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+  var newArray = string.split("");
+  let reverseArray = newArray.reverse();
+   let newString = reverseArray.join("");
+   return newString;
+ };
 
 // Do not edit below this line
 module.exports = reverseString;
