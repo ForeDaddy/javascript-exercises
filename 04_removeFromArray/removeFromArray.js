4eed5999e89fdac036aa182289c5fbd2622b9165
Donn Neufeld <69326497+ForeDaddy@users.noreply.github.com>
2022-11-23 09:38:45 -0800

    Test 1 submission

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, value) {
+  if (array.includes(value) === true) {
+   let valueIndex = array.indexOf(value);
+   
+    array.splice(valueIndex, 1);
+    return array;
+  } else {
+    return array;
+  }
+   
+ };
 
 // Do not edit below this line
 module.exports = removeFromArray;
